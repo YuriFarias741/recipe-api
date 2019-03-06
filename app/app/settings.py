a321c74abf48b570e762fb78db0cb286395708c7
Yuri Farias <yuricoelhodefarias@hotmail.com>
2019-03-05 22:29:42 -0300

    Fixes typo on settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backens.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
